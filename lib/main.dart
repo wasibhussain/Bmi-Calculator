56a8b59ddf9ca306eef08b3aa99ba6b96dd72f71
ashfaq hussain <theashfaqahmads@gmail.com>
2022-12-12 23:34:07 +0500

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:bmi/Screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'Screens/input_page.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
